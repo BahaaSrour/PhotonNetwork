8c3f365bde7459947a84776f027c2020c634ae26
BahaaSrour <dev.bahaasrour@gmail.com>
2021-03-18 17:59:11 +0200

    -Connect to network works fine

---
+++ Assets/Application Asset/Scripts/Launcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    [SerializeField] GameObject connectedScreen;
+    [SerializeField] GameObject disconnectedScreen;
+
+
+    public void OnClick_ConnectBtn()
+    {
+        PhotonNetwork.ConnectUsingSettings(); 
+    }
+
+    public override void OnConnectedToMaster()
+    { 
+        connectedScreen.SetActive(true);
+    }
+
+   
+}
